b489484c0db7b1b60c2079db0d0b4dcfb25d3872
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-23 21:49:49 +0900

    create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,39 @@
 import * as React from "react";
 import styled from "react-emotion";
-import { transparentize } from "polished";
 import { Link } from "gatsby";
 
-import { heights, dimensions, colors } from "../styles/variables";
-import Container from "./Container";
+import { heights, dimensions } from "../styles/variables";
+
+import logo from "../assets/images/co-ba-logo.png";
 
 const StyledHeader = styled.header`
   height: ${heights.header}px;
   padding: 0 ${dimensions.containerPadding}rem;
-  background-color: ${colors.brand};
-  color: ${transparentize(0.5, colors.white)};
+  border-bottom: solid 1px;
 `;
 
-const HeaderInner = styled(Container)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+const Logo = styled.img`
   height: 100%;
+  padding: 5px 0;
+  float: left;
+  filter: invert(100%);
 `;
 
-const HomepageLink = styled(Link)`
-  color: ${colors.white};
-  font-size: 1.5rem;
-  font-weight: 600;
+const NavLinksWrapper = styled.ul`
+  float: right;
+  list-style: none;
+`;
+
+const NavLink = styled.li`
+  float: left;
+  font-size: 35px;
+  line-height: ${heights.header}px;
+  vertical-align: middle;
+  margin: 0 50px;
+`;
+
+const LinkWrapper = styled(Link)`
+  color: #000;
 
   &:hover,
   &:focus {
@@ -31,15 +41,25 @@ const HomepageLink = styled(Link)`
   }
 `;
 
-interface HeaderProps {
-  title: string;
-}
-
-const Header: React.SFC<HeaderProps> = ({ title }) => (
+const Header: React.SFC = () => (
   <StyledHeader>
-    <HeaderInner>
-      <HomepageLink to="/">{title}</HomepageLink>
-    </HeaderInner>
+    <LinkWrapper to="/">
+      <Logo src={logo} />
+    </LinkWrapper>
+    <NavLinksWrapper>
+      <NavLink>
+        <LinkWrapper to="/history">History</LinkWrapper>
+      </NavLink>
+      <NavLink>
+        <LinkWrapper to="/events">Events</LinkWrapper>
+      </NavLink>
+      <NavLink>
+        <LinkWrapper to="/photos">Photos</LinkWrapper>
+      </NavLink>
+      <NavLink>
+        <LinkWrapper to="/access">Access</LinkWrapper>
+      </NavLink>
+    </NavLinksWrapper>
   </StyledHeader>
 );
 
