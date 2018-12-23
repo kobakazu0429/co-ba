01ba682304fd20497b1f5c9ccb05d67ecd11067c
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-23 21:49:54 +0900

    create pages

---
+++ src/pages/history.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import Page from "../components/Page";
+import Container from "../components/Container";
+import IndexLayout from "../layouts";
+
+const IndexPage = () => (
+  <IndexLayout>
+    <Page>
+      <Container>
+        <h1>History</h1>
+      </Container>
+    </Page>
+  </IndexLayout>
+);
+
+export default IndexPage;
