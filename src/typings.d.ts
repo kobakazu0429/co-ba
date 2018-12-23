5042cea56b94c6fc81cc17de35e505ac662f2d37
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-23 21:21:37 +0900

    add image, font types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -13,3 +13,12 @@ declare module "*.module.css" {
   const cssModule: CSSModule;
   export = cssModule;
 }
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.gif";
+declare module "*.eot";
+declare module "*.svg";
+declare module "*.ttf";
+declare module "*.woff";
+declare module "*.woff2";
