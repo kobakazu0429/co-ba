63c9f68f19246e0761f3c0b24d786c75fe945c08
kobakakzu0429 <kobakazu10@gmail.com>
2019-02-17 00:44:39 +0900

    add media query, breakpoints

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -29,11 +29,18 @@ export const fonts = {
 };
 
 export const breakpoints = {
+  mobile: "40.0rem",
+  tablet: "80.0rem",
   xs: 0,
   sm: 576,
   md: 768,
   lg: 992,
   xl: 1200
+};
+
+export const media = {
+  mobile: `screen and (max-width: ${breakpoints.mobile})`,
+  tablet: `screen and (min-width: ${breakpoints.mobile}) and (max-width: ${breakpoints.tablet})`
 };
 
 export const widths = {
@@ -61,5 +68,5 @@ export const dimensions = {
 };
 
 export const heights = {
-  header: 60
+  header: 50
 };
